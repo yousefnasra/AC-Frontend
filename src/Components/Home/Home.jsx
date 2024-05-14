4bb9fdd4a411540faadc7ff7625511d9db76073c
yousefnasra <yosefnasra96@gmail.com>
2024-05-14 12:15:04 +0300

    fix title name

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Helmet>
-        <title>FreshCart home</title>
+        <title>home</title>
       </Helmet>
       <MainSlider></MainSlider>
       <CategorySlider></CategorySlider>
